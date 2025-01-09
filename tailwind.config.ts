0f7c369994ad1e57508b0bac204f6a18bff917d3
MisaJC <johncarlomisa399@gmail.com>
2025-01-09 17:02:48 +0800

    initialize a new project for an ai logo generator

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,10 +11,75 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "#131315",
+        foreground: "#EEEEF1",
+        card: {
+          DEFAULT: "#1C1C21",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        dark: {
+          DEFAULT: "#131315",
+          100: "#C3C3C6",
+          200: "#0B0B0C",
+          300: "#232325",
+          400: "#090909",
+          500: "#4A4A4D",
+          600: "#070708",
+          700: "#19191A",
+          800: "#29292B",
+          900: "#0D0D0E",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        primary: {
+          DEFAULT: "#DB1A5A",
+          100: "#EC4B7B",
+          200: "#C01750",
+          300: "#E04368",
+          400: "#AB1345",
+          500: "#F07397",
+          600: "#940F3B",
+          700: "#E04343",
+          800: "#D02D72",
+          900: "#980022",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "#131315",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
